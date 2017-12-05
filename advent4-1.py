ba6a2018e3d5ca1530088ddbe7c4aaf61f8bb007
Esti Shay <esti.r.shay@gmail.com>
2017-12-04 23:06:49 -0800

    complete part 1 of day 4

---
+++ advent4-1.py
@@ -0,0 +1,37 @@
+import csv
+
+adventFile = open('advent4Data.csv')
+adventReader = csv.reader(adventFile)
+adventData = list(adventReader)
+
+def passphrases1(passList):
+    valid = 0
+    invalid = 0
+
+    for entry in passList:
+        print(entry)
+        entryWords = entry[0].split(" ")
+        index1 = 0
+        index2 = 1
+        check = True
+
+        while check == True and index2 < len(entryWords):
+            index3 = index2
+            while check == True and index3 < len(entryWords):
+                if entryWords[index1] == entryWords[index3]:
+                    check = False
+                    invalid += 1
+                else:
+                    index3 += 1
+
+            index1 += 1
+            index2 += 1
+
+            if index2 == len(entryWords)-1 and check == True:
+                valid += 1
+                print(entryWords)
+
+
+    print("There are "+str(valid)+" valid and "+str(invalid)+" invalid passphrases.")
+
+passphrases1(adventData)
